494d93e241c5586ca3fda16572c0786d57211529
bjdzliu <ldz_cll@hotmail.com>
2017-07-04 11:12:37 +0800

    mysql connection example

--- www/config.py
+++ www/config.py
@@ -1,26 +1,67 @@
 import config_default
+import sys
+from collections import Iterable
 
+print(sys.path)
 
 class Dict(dict):
-    def __init__(self,name=(),values=(),**kw):
+    def __init__(self,names=(),values=(),**kw):
         super(Dict,self).__init__(**kw)
-        for k,v in zip(names,values):
+        for k,v in zip(names, values):
             self[k]=v
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
             raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
-        def __setattr__(self, key, value):
+    def __setattr__(self, key, value):
             self[key]=value
 
+def merge(defaults, override):
+    r={}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k]=merge(v,override[k])
+            else :
+                r[k]=override[k]
 
-import config_override.configs
+        else:
+            r[k] = v
+    # for k,v in r.items():
+    #     print('r',k,v)
+    return r
 
-def test():
-    for k in config_override:
-        print(k)
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    # for k,v in D.items():
+    #     if(k == "session") :
+    #         print("session is k",v)
+    #     else:
+    #         print('DDDDDDD',k)
+    return D
 
-test()
+configs=config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+    # for k, v in configs.items():
+    #     print(k,v)
+except ImportError:
+     pass
+
+configs = toDict(configs)
+print(configs['db'])
+
+
+
+def get_configdefault():
+    for k in config_override.configs:
+        print("config_override.configs's key and value is ",k,config_override.configs[k])
+
+#get_configdefault()
 
 
--- www/config_default.py
+++ www/config_default.py
@@ -3,9 +3,9 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
-        'db': 'awesome'
+        'user': 'xiaoli',
+        'password': 'mypassword',
+        'dbname': 'awesome'
     },
     'session': {
         'secret': 'Awesome'
